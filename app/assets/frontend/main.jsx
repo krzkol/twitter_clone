b684b07e72968e49df13e2dd165f567c480791e5
Krzysztof Kołek <krzysztof.kolek@netguru.pl>
2016-03-12 18:55:15 +0100

    add twitter clone files to git

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,49 @@
+import TweetBox from './components/TweetBox'
+import TweetsList from './components/TweetsList'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tweetsList: [] };
+  }
+  formattedTweets(tweetsList) {
+    let formattedList = tweetsList.map(tweet => {
+      tweet.formattedDate = moment(tweet.created_at).fromNow();
+      return tweet;
+    });
+    return {
+      tweetsList: formattedList
+    };
+  }
+  addTweet(tweetToAdd) {
+    $.post('/tweets', { body: tweetToAdd })
+    .success(savedTweet => {
+      let newTweetsList = this.state.tweetsList;
+      newTweetsList.unshift(savedTweet);
+      this.setState(this.formattedTweets(newTweetsList));
+    })
+    .error(error => console.log(error));
+  }
+  componentDidMount() {
+    $.ajax('/tweets')
+    .success(data => this.setState(this.formattedTweets(data)))
+    .error(error => console.log(error));
+  }
+  render() {
+    return (
+      <div className="container">
+        <TweetBox sendTweet={this.addTweet.bind(this)}/>
+        <TweetsList tweets={this.state.tweetsList} />
+      </div>
+    );
+  }
+}
+
+let documentReady = () => {
+  let reactNode = document.getElementById('react');
+  if(reactNode) {
+    ReactDOM.render(<Main />, reactNode);
+  }
+};
+
+$(documentReady);
